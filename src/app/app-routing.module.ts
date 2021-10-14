c433904b864d099369b01d8560000ac9eb190a15
Akanksha Dumpeti <dumpetiakanksha@gmail.com>
2021-10-14 21:49:58 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AttendanceReportComponent } from './attendance-report/attendance-report.component';
+import { AttendanceComponent } from './attendance/attendance.component';
+import { CreateStudentComponent } from './create-student/create-student.component';
+import { StudentEditComponent } from './student-edit/student-edit.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "create-student",
+    component : CreateStudentComponent
+  },
+  {
+    path : "student-list",
+    component : StudentListComponent
+  },
+  {
+    path : "student-edit/:id",
+    component : StudentEditComponent
+  },
+  {
+    path : "attendance",
+    component : AttendanceComponent
+  },
+  {
+    path : "attendance-report",
+    component : AttendanceReportComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CreateStudentComponent } from './create-student/create-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentEditComponent } from './student-edit/student-edit.component';
+import { AttendanceComponent } from './attendance/attendance.component';
+import { AttendanceReportComponent } from './attendance-report/attendance-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    CreateStudentComponent,
+    StudentListComponent,
+    StudentEditComponent,
+    AttendanceComponent,
+    AttendanceReportComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
